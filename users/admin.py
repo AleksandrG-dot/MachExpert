1d7b1ad5edc989ffdafb90af7caed7be0582e607
Aleksandr Goncharov <potta@inbox.ru>
2025-10-18 00:23:53 +0500

    Add migration users + admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Employee
 
-# Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(UserAdmin):
+    list_display = ('username', 'last_name', 'first_name', 'patronymic', 'position', 'service_number')
+    list_filter = ('position', 'groups')
+    search_fields = ('last_name', 'first_name', 'patronymic', 'position',  'service_number')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительная информация', {
+            'fields': ('patronymic', 'service_number', 'internal_phone', 'mobile_phone',
+                       'position', 'birthday', 'date_employment', 'date_dismissal', 'comment')
+        }),
+    )
